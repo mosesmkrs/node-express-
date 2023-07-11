2b2a6f5232b0451409b6e899c48f212c06106fc0
mosesmkrs <karisamoses392@gmail.com>
2023-07-11 10:06:53 +0300

    first commit

---
+++ 3-module.js
@@ -0,0 +1,10 @@
+//modules
+const data = require("./6-alt flavor")
+const names = require("./4-names")
+const sayHi = require("./5-utilities")
+require("./7-mind-greanade")
+
+// console.log(data)
+// sayHi("susan")
+// sayHi(names.john)
+// sayHi(names.jack)
